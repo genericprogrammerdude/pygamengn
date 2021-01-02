21e83e3c043cc9dc6b3310454df6c1460818b809
Ernesto Novillo <ernesto_novillo@intuit.com>
2021-01-02 15:41:40 -0500

    Moved SpaceShooter files out of GameEngine.

---
+++ ship.py
@@ -0,0 +1,43 @@
+from game_object import GameObject
+from game_object_factory import GameObjectFactory
+from health_bar import HealthBar
+from mover import MoverVelocity
+from projectile import Projectile
+
+
+@GameObjectFactory.register("Ship")
+class Ship(GameObject):
+    """Space ship game object."""
+
+    def __init__(self, image, projectile_type, enemies, fire_freq, mover, **kwargs):
+        super().__init__(image, **kwargs)
+        self.mover = mover
+        self.projectile_type = projectile_type
+        self.enemies = enemies
+        self.fire_freq = fire_freq
+        self.time_since_last_fire = self.fire_freq
+
+    def update(self, delta):
+        """Updates the ship."""
+        # Translate according to velocity
+        self.pos, self.heading = self.mover.move(delta, self.pos, self.heading)
+        # Now do the regular GameObject update
+        super().update(delta)
+        self.time_since_last_fire += delta
+
+    def set_velocity(self, velocity):
+        """Sets the ship's velocity."""
+        self.mover.set_velocity(velocity)
+
+    def fire(self):
+        """Fires a Projectile at the target."""
+        if self.time_since_last_fire > self.fire_freq and self.alive():
+            projectile = GameObjectFactory.create(self.projectile_type, enemies=self.enemies)
+            projectile.set_pos(self.pos)
+            projectile.set_heading(self.heading)
+            projectile.transform()
+
+            group = self.groups()[0]
+            group.add(projectile)
+
+            self.time_since_last_fire = 0
---
+++ turret.py
@@ -0,0 +1,50 @@
+import math
+import random
+
+from game_object import GameObject
+from game_object_factory import GameObjectFactory
+
+
+@GameObjectFactory.register("Turret")
+class Turret(GameObject):
+    """Turret that will fire at the given target."""
+
+    def __init__(self, image, projectile_type, enemies, fire_freq, **kwargs):
+        super().__init__(image, **kwargs)
+        self.projectile_type = projectile_type
+        self.target = None
+        self.fire_freq = fire_freq
+        self.time_since_last_fire = random.randint(0, fire_freq)
+        self.enemies = enemies
+
+    def set_target(self, target):
+        """Sets the target to attack."""
+        self.target = target
+
+    def update(self, delta):
+        super().update(delta)
+
+        if self.target:
+            if self.target.alive():
+                fire_dir = self.target.pos - self.pos
+                heading = math.degrees(math.atan2(fire_dir[0], fire_dir[1]) - math.pi)
+                self.set_heading(heading)
+
+                self.time_since_last_fire = self.time_since_last_fire + delta
+                if self.time_since_last_fire >= self.fire_freq and self.alive():
+                    self.fire()
+            else:
+                self.time_since_last_fire = self.fire_freq
+                self.target = None
+
+    def fire(self):
+        """Fires a projectile_type object at the target."""
+        projectile = GameObjectFactory.create(self.projectile_type, enemies=self.enemies)
+        projectile.set_pos(self.pos)
+        projectile.set_heading(self.heading)
+        projectile.transform()
+
+        group = self.groups()[0]
+        group.add(projectile)
+
+        self.time_since_last_fire = 0
