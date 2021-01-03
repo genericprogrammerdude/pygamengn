ec1b3cd9ecaafa2274e1070492099e96c3880fed
Ernesto Novillo <ernesto_novillo@intuit.com>
2021-01-03 14:39:10 -0500

    Improved Asteroid randomness and spawning.

--- asteroid.py
+++ asteroid.py
@@ -9,13 +9,17 @@ from projectile import Projectile
 @GameObjectFactory.register("Asteroid")
 class Asteroid(Projectile):
 
-    def __init__(self, image, damage, death_effect, mover, death_spawn):
-        super().__init__(image, damage, death_effect, mover)
-        self.spin_angle = 0
+    def __init__(self, images, damage, death_effect, mover, death_spawn):
+        super().__init__(random.choice(images), damage, death_effect, mover)
+        self.spin_angle = random.randrange(0, 360)
+        self.spin_delta_factor = random.choice([-1.0, 1.0])
         self.death_spawn = death_spawn
+        self.death_spawn_pos = None
+        self.mover.set_velocity(self.mover.velocity * (0.5 + random.random()))
 
     def update(self, delta):
-        self.spin_angle = (self.spin_angle + 1) % 360
+        spin_delta = (self.mover.angular_velocity * delta) / 1000.0 * self.spin_delta_factor
+        self.spin_angle = self.spin_angle + spin_delta
         self.pos, self.heading = self.mover.move(delta, self.pos, self.heading)
         self.image = pygame.transform.rotozoom(self.image_original, self.spin_angle, self.scale)
         self.mask = pygame.mask.from_surface(self.image, 16)
@@ -27,6 +31,7 @@ class Asteroid(Projectile):
 
     def handle_collision(self, gob, world_pos):
         """Reacts to collision against game object gob."""
+        self.death_spawn_pos = world_pos
         self.take_damage(self.health)
         # Apply damage to the collided sprite
         gob.take_damage(self.damage)
@@ -37,10 +42,8 @@ class Asteroid(Projectile):
             for spawn_type in self.death_spawn:
                 spawn = GameObjectFactory.create(spawn_type)
                 spawn.add_to_groups(self.groups())
-                spawn.set_pos(pygame.Vector2(640, 370))
-                heading_delta = 60.0 * (random.random() - 0.5)
+                spawn.set_pos(self.death_spawn_pos)
+                heading_delta = 90.0 * (random.random() - 0.5)
                 spawn.set_heading(self.heading + heading_delta)
-                velocity = self.mover.velocity * (0.5 + random.random())
-                spawn.mover.set_velocity(velocity)
                 spawn.transform()
         super().die()
