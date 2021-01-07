838fff8197c5f8dd64305a4a2ef06ec3ea6748be
Ernesto Novillo <ernesto_novillo@intuit.com>
2021-01-06 20:09:04 -0500

    Check that sprites are alive before checking for mask collisions.

--- GameEngine/collision_manager.py
+++ GameEngine/collision_manager.py
@@ -19,12 +19,13 @@ class CollisionManager(GameObjectBase):
         collisions = pygame.sprite.groupcollide(group_a, group_b, False, False, self.collided)
         for gob_a in collisions.keys():
             for gob_b in collisions[gob_a]:
-                collision = pygame.sprite.collide_mask(gob_a, gob_b)
-                if collision:
-                    # Get world position of collision point for colliding GameObjects to know
-                    world_pos = pygame.Vector2(gob_a.rect.topleft) + collision
-                    gob_b.handle_collision(gob_a, world_pos)
-                    gob_a.handle_collision(gob_b, world_pos)
+                if gob_a.alive() and gob_b.alive():
+                    collision = pygame.sprite.collide_mask(gob_a, gob_b)
+                    if collision:
+                        # Get world position of collision point for colliding GameObjects to know
+                        world_pos = pygame.Vector2(gob_a.rect.topleft) + collision
+                        gob_b.handle_collision(gob_a, world_pos)
+                        gob_a.handle_collision(gob_b, world_pos)
 
     def collided(self, a, b):
         """Checks whether sprites a and b collide."""
