300d5d73e4d3dc3c20be1024eda5603081e98bf2
Ernesto Novillo <ernesto_novillo@intuit.com>
2021-01-14 22:09:30 -0500

    Added TextPanel vertical and horizontal alignment.

--- GameEngine/UI/panel.py
+++ GameEngine/UI/panel.py
@@ -1,3 +1,5 @@
+from enum import Enum, auto
+
 import pygame
 
 from UI.ui_base import UIBase
@@ -37,10 +39,22 @@ class ColourPanel(UIBase):
 class TextPanel(UIBase):
     """Panel that sets its size to the size of the text in it. This panel ignores the parent rect."""
 
-    def __init__(self, font_asset, text_colour, text, **kwargs):
+    class VertAlign(Enum):
+        TOP = "TOP"
+        CENTRE = "CENTRE"
+        BOTTOM = "BOTTOM"
+
+    class HorzAlign(Enum):
+        LEFT = "LEFT"
+        CENTRE = "CENTRE"
+        RIGHT = "RIGHT"
+
+    def __init__(self, font_asset, text_colour, horz_align, vert_align, text, **kwargs):
         super().__init__(**kwargs)
         self.font_asset = font_asset
         self.text_colour = text_colour
+        self.horz_align = TextPanel.HorzAlign(horz_align)
+        self.vert_align = TextPanel.VertAlign(vert_align)
         self.text = text
         self.text_is_dirty = True
 
@@ -55,7 +69,44 @@ class TextPanel(UIBase):
         """TextPanel ignores its parent rect and renders to the font size."""
         self.image = self.font_asset.font.render(self.text, True, self.text_colour)
         image_size = self.image.get_rect().size
-        if image_size[0] > self.parent_rect.size[0] or image_size[1] > self.parent_rect.size[1]:
+        if image_size[0] != self.parent_rect.size[0] or image_size[1] != self.parent_rect.size[1]:
             size = self.scale_to_fit(self.image.get_rect().size, self.parent_rect.size)
             self.image = pygame.transform.scale(self.image, size)
         self.text_is_dirty = False
+        self.align()
+
+    def align(self):
+        """Aligns the text image."""
+        # Horizontal alignment
+        if self.horz_align == TextPanel.HorzAlign.LEFT:
+            pass  # This is what UIBase does by default
+        elif self.horz_align == TextPanel.HorzAlign.CENTRE:
+            self.rect.x = self.rect.x + (self.rect.width - self.image.get_rect().width) / 2
+        elif self.horz_align == TextPanel.HorzAlign.RIGHT:
+            self.rect.x = self.rect.x + self.rect.width - self.image.get_rect().width
+        # Vertical alignment
+        if self.vert_align == TextPanel.VertAlign.TOP:
+            pass  # This is what UIBase does by default
+        elif self.vert_align == TextPanel.VertAlign.CENTRE:
+            self.rect.y = self.rect.y + (self.rect.height - self.image.get_rect().height) / 2
+        elif self.vert_align == TextPanel.VertAlign.BOTTOM:
+            self.rect.y = self.rect.y + self.rect.height - self.image.get_rect().height
+
+    @classmethod
+    def scale_to_fit(cls, size, max_size):
+        """Scales a rectangle size down to fit max_size maintaining the original aspect ratio."""
+        width = size[0]
+        height = size[1]
+        scale_x = max_size[0] / width
+        scale_y = max_size[1] / height
+
+        if scale_x < 1 or scale_y < 1:
+            aspect_ratio = width / height
+            if scale_x < scale_y:
+                width = max_size[0]
+                height = width / aspect_ratio
+            else:
+                height = max_size[1]
+                width = height * aspect_ratio
+
+        return (round(width), round(height))
