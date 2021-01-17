88609ee5ae4f50764f646735bda73cff3934ea71
Ernesto Novillo <ernesto_novillo@intuit.com>
2021-01-16 21:14:15 -0500

    Added an AsteroidSpawner to MainMenu.

--- main_menu.py
+++ main_menu.py
@@ -2,6 +2,7 @@ import pygame
 
 from UI.font_asset import FontAsset
 from UI.panel import ColourPanel
+from asteroid import AsteroidSpawner
 from game import Game
 from game_object_factory import GameObjectFactory
 
@@ -10,11 +11,17 @@ from game_object_factory import GameObjectFactory
 class MainMenu(ColourPanel):
     """Main menu UI."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, asteroid_spawner, **kwargs):
         super().__init__(**kwargs)
+        self.asteroid_spawner = asteroid_spawner
         self.start_button = self.children[0]
         self.exit_button = self.children[1]
         self.game = None
+
+    def update(self, parent_rect, delta):
+        """Updates the main menu."""
+        super().update(parent_rect, delta)
+        self.asteroid_spawner.update(delta)
 
     def set_game(self, game):
         """Sets the instance of Game to interact with."""
