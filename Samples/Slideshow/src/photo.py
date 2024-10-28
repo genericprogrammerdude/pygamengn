1a3289f9b0900013d94bf7a836f9f5109c573336
Ernesto Novillo <ernesto.novillo@gmail.com>
2024-10-28 11:44:42 -0400

    Pre-loading all photos to avoid hitches and using GameObject.visible to control when a photo becomes actively updated. Also added code to identify photos that scale up higher than 1.0.

--- Samples/Slideshow/src/photo.py
+++ Samples/Slideshow/src/photo.py
@@ -14,6 +14,8 @@ class Photo(GameObject):
     def __init__(self, mover, **kwargs):
         super().__init__(**kwargs)
         self.mover = mover
+        self.is_active = False
+        self.max_scale = 1.0
 
         # Get maximum scale so that the photo fits the screen
         screen_size = pygame.display.get_surface().get_rect().size
@@ -26,8 +28,9 @@ class Photo(GameObject):
         self.set_scale(self.max_scale / 2.0)
 
     def update(self, delta):
-        super().update(delta)
-        self.position = self.position + self.mover.move(delta)
+        if self.visible:
+            super().update(delta)
+            self.position = self.position + self.mover.move(delta)
 
 
 @ClassRegistrar.register("PhotoSpawner")
@@ -44,6 +47,59 @@ class PhotoSpawner(Updatable):
         self.photo_index = 0
         self.done = False
 
+        screen_size = pygame.display.get_surface().get_rect().size
+        self.photos = []
+        photo_index = 0
+        for image in self.images:
+            photo = self.photo_type_spec.create(image_asset = image)
+            if photo.max_scale > 1.0:
+                print(f"*** Small photo! {photo.max_scale:.1f} {photo_index:03}")
+            photo_index += 1
+            self.photos.append(photo)
+
+# Small photos
+# *** Small photo! 2.7 062
+# *** Small photo! 1.2 095
+# *** Small photo! 1.0 098
+# *** Small photo! 1.1 102
+# *** Small photo! 2.2 111
+# *** Small photo! 1.8 112
+# *** Small photo! 1.2 118
+# *** Small photo! 1.1 122
+# *** Small photo! 1.4 134
+# *** Small photo! 1.6 145
+# *** Small photo! 1.0 168
+# *** Small photo! 1.0 169
+# *** Small photo! 1.4 177
+# *** Small photo! 1.8 178
+# *** Small photo! 1.8 179
+# *** Small photo! 1.8 180
+# *** Small photo! 1.8 181
+# *** Small photo! 1.8 182
+# *** Small photo! 1.8 183
+# *** Small photo! 1.8 184
+# *** Small photo! 1.8 185
+# *** Small photo! 1.8 186
+# *** Small photo! 1.8 187
+# *** Small photo! 1.8 188
+# *** Small photo! 1.3 189
+# *** Small photo! 1.8 190
+# *** Small photo! 1.4 191
+# *** Small photo! 1.8 192
+# *** Small photo! 1.4 193
+# *** Small photo! 1.3 194
+# *** Small photo! 1.4 195
+# *** Small photo! 1.8 196
+# *** Small photo! 1.4 197
+# *** Small photo! 1.4 198
+# *** Small photo! 1.7 199
+# *** Small photo! 1.4 202
+# *** Small photo! 1.5 203
+# *** Small photo! 1.4 204
+# *** Small photo! 1.0 216
+# *** Small photo! 1.7 223
+# *** Small photo! 1.5 226
+
     def update(self, delta):
         self.total_time += delta
         self.time_to_next_spawn -= delta
@@ -51,8 +107,10 @@ class PhotoSpawner(Updatable):
         if self.time_to_next_spawn <= 0:
             self.time_to_next_spawn = self.spawn_freq
 
-            # Spawn new photo
-            photo = self.photo_type_spec.create(image_asset = self.images[self.photo_index])
+            # Activate new photo
+            photo = self.photos[self.photo_index]
+            photo.visible = True
+            # photo = self.photo_type_spec.create(image_asset = self.images[self.photo_index])
             pos, direction = self.get_random_pos_dir(self.render_group.get_world_view_rect())
             photo.mover.set_direction(direction)
             photo.position = pos
