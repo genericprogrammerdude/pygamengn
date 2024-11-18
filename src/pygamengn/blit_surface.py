e47b4d3a35f738f9d28b6374267f9090b20dabc9
Ernesto Novillo <ernesto.novillo@gmail.com>
2024-11-17 22:50:30 -0500

    Made BlitSurface.topleft a Vector2.

--- src/pygamengn/blit_surface.py
+++ src/pygamengn/blit_surface.py
@@ -4,7 +4,7 @@ import pygame
 class BlitSurface:
     """Specification for a surface that will be blitted while rendering."""
 
-    def __init__(self, surface: pygame.Surface, topleft: tuple[int]):
+    def __init__(self, surface: pygame.Surface, topleft: pygame.Vector2):
         """topleft is in screen coordinates."""
         self.surface = surface
         self.topleft = topleft
